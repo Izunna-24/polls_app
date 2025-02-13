a6a568f92afe565485c3784fb1942a58964ece9a
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2025-02-13 09:17:56 +0100

    userprincipal implemeneted

--- src/main/java/com/glentfoundation/polls/security/UserPrincipal.java
+++ src/main/java/com/glentfoundation/polls/security/UserPrincipal.java
@@ -1,12 +1,23 @@
 package com.glentfoundation.polls.security;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.glentfoundation.polls.models.User;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserPrincipal implements UserDetails {
+    @EqualsAndHashCode.Include
     private Long id;
     private String username;
     private String name;
@@ -15,4 +26,37 @@ public class UserPrincipal implements UserDetails {
     @JsonIgnore
     private String password;
     private Collection<? extends GrantedAuthority> authorities;
+
+    public static UserPrincipal create(User user) {
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toList());
+        return new UserPrincipal(
+                user.getId(),
+                user.getName(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities
+        );
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
