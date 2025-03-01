7d661c734c9b0c1e60c8e54c7679141c67bfef56
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2025-03-01 19:29:27 +0100

    creating User date Audit

---
+++ src/main/java/com/glentfoundation/polls/models/audit/UserDateAudit.java
@@ -0,0 +1,26 @@
+package com.glentfoundation.polls.models.audit;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
+@MappedSuperclass
+@JsonIgnoreProperties(
+        value = {"createdBy", "updatedBy"},
+        allowGetters = true
+)
+@Getter
+@Setter
+public abstract class UserDateAudit extends DateAudit {
+    @CreatedBy
+    @Column(updatable = false)
+    private Long createdBy;
+
+    @LastModifiedBy
+    private Long updatedBy;
+}
