c73424bb718df0c7b4d27ecb482461b6280913b1
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2025-03-01 23:49:13 +0100

    Vote business model fixed

---
+++ src/main/java/com/glentfoundation/polls/models/Vote.java
@@ -0,0 +1,37 @@
+package com.glentfoundation.polls.models;
+
+import com.glentfoundation.polls.models.audit.DateAudit;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="votes", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+                "poll_id",
+                "user_id"
+        })
+})
+
+@Getter
+@Setter
+public class Vote extends DateAudit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "poll_id", nullable = false)
+    private Poll poll;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "choice_id", nullable = false)
+    private Choice choice;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+
+}
