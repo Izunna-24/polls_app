29aeeb7369f1ac7b9e88e6ee112e63a42ddce823
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2025-03-01 16:31:45 +0100

    registeration controllers

--- src/main/java/com/glentfoundation/polls/controller/AuthController.java
+++ src/main/java/com/glentfoundation/polls/controller/AuthController.java
@@ -1,21 +1,34 @@
 package com.glentfoundation.polls.controller;
 
+import com.glentfoundation.polls.exceptions.AppException;
+import com.glentfoundation.polls.exceptions.ResourceNotFoundException;
+import com.glentfoundation.polls.models.Role;
+import com.glentfoundation.polls.models.RoleName;
+import com.glentfoundation.polls.models.User;
 import com.glentfoundation.polls.payload.requests.LoginRequest;
+import com.glentfoundation.polls.payload.requests.SignUpRequest;
+import com.glentfoundation.polls.payload.requests.responses.ApiResponse;
 import com.glentfoundation.polls.payload.requests.responses.JwtAuthenticationResponse;
 import com.glentfoundation.polls.repository.RoleRepositry;
 import com.glentfoundation.polls.repository.UserRepository;
 import com.glentfoundation.polls.security.JwtTokenProvider;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -31,6 +44,7 @@ public class AuthController {
     @Autowired
     JwtTokenProvider jwtTokenProvider;
 
+    @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
@@ -41,6 +55,30 @@ public class AuthController {
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String jwt  = jwtTokenProvider.generateToken(authentication);
         return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
+        if(userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return new ResponseEntity<>(new ApiResponse(false,"username already taken"), HttpStatus.BAD_REQUEST);
+        }
+        if(userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return new ResponseEntity<>(new ApiResponse(false,"email already in use!"), HttpStatus.BAD_REQUEST);
+        }
+
+    User user = new User(signUpRequest.getName(), signUpRequest.getEmail(),
+            signUpRequest.getUsername(), signUpRequest.getPassword());
+    user.setPassword(passwordEncoder.encode(user.getPassword()));
+    Role userRole = roleRepositry.findByName(RoleName.ROLE_USER)
+            .orElseThrow(() -> new AppException("User Role Not Set!!"));
+    user.setRoles(Collections.singleton(userRole));
+    User savedUser = userRepository.save(user);
+
+    URI location = ServletUriComponentsBuilder.fromCurrentContextPath()
+            .path("/api/users/{username}")
+            .buildAndExpand(savedUser.getUsername()).toUri();
+    return ResponseEntity.created(location).body(new ApiResponse(true,"User registered successfully"));
 
     }
 
--- src/main/java/com/glentfoundation/polls/models/User.java
+++ src/main/java/com/glentfoundation/polls/models/User.java
@@ -6,20 +6,20 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.NaturalId;
 
 import java.util.HashSet;
 import java.util.Set;
+
 
 @Entity
 @Table(name= "users", uniqueConstraints = {
         @UniqueConstraint(columnNames = {"username"}),
         @UniqueConstraint(columnNames = {"email"})
 })
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class User extends DateAudit {
@@ -51,5 +51,12 @@ public class User extends DateAudit {
         joinColumns = @JoinColumn(name="user_id"),
         inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
+
+    public User(String name, String username, String email, String password) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 
 }
