1ff86c6ded932e0091849cb9a64dbd8d9f26ca83
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2025-03-01 14:52:50 +0100

    payload requests built

--- src/main/java/com/glentfoundation/polls/payload/requests/SignUpRequest.java
+++ src/main/java/com/glentfoundation/polls/payload/requests/SignUpRequest.java
@@ -1,9 +1,29 @@
 package com.glentfoundation.polls.payload.requests;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class SignUpRequest {
+    @NotBlank
+    @Size(min = 4, max = 40)
     private String name;
+
+
+    @NotBlank
+    @Size(max = 20)
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min = 3, max = 20)
     private String username;
 
+    @NotBlank
+    @Size(min = 6, max = 16)
     private String password;
 }
