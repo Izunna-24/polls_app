282f4d40325dc1a25f007434a768ee237d2de5dd
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2025-03-02 12:05:29 +0100

    Creating User Controller

---
+++ src/main/java/com/glentfoundation/polls/controllers/UserController.java
@@ -0,0 +1,43 @@
+package com.glentfoundation.polls.controllers;
+
+import com.glentfoundation.polls.payload.requests.responses.UserSummary;
+import com.glentfoundation.polls.repository.PollRepository;
+import com.glentfoundation.polls.repository.UserRepository;
+import com.glentfoundation.polls.repository.VoteRepository;
+import com.glentfoundation.polls.security.CurrentUser;
+import com.glentfoundation.polls.security.UserPrincipal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PollRepository pollRepository;
+    @Autowired
+    private VoteRepository voteRepository;
+    @Autowired
+    private PollService pollService;
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    public UserSummary getCurrentUser(@CurrentUser UserPrincipal currentUser) {
+        UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getName());
+        return userSummary;
+    }
+
+
+
+
+}
